0ed65b591db6ec06a9916e7463b751156a1608d7
kuritayu <summergirlna@gmail.com>
2026-08-30 14:04:05 +0900

    Add basic authentication for BFF and configure security
    Integrate Spring Security into the BFF application with basic authentication to secure GraphQL and GraphiQL endpoints. Update Docker configuration and frontend client to support authentication via environment variables. Ensure sensitive credentials are configurable and set appropriate permissions for various endpoints.

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,5 +1,15 @@
 import {GraphQLClient} from "graphql-request";
 
 const endpoint = import.meta.env.VITE_GRAPHQL_ENDPOINT ?? 'http://localhost:8082/graphql';
+const username = import.meta.env.VITE_BFF_BASIC_AUTH_USERNAME;
+const password = import.meta.env.VITE_BFF_BASIC_AUTH_PASSWORD;
 
-export const graphqlClient = new GraphQLClient(endpoint);
+const headers =
+    username && password ?
+        {
+            Authorization: `Basic ${btoa(`${username}:${password}`)}`
+        } : undefined;
+
+export const graphqlClient = new GraphQLClient(endpoint, {
+    headers
+});
